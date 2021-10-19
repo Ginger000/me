19783a6293811d94286c2df1b69d024c79a5c908
maomao <maomao@missionbot.cn>
2021-10-19 13:56:14 +0800

    ⭐️init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Me</p>
+    </div>
+  );
+}
+
+export default hot(module)(App);
