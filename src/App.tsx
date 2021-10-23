0c43ccec168355c932a4b9e34a8e7429298133f9
FionaHua <405712507@qq.com>
2021-10-23 09:39:56 +0800

    test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import './App.scss';
-import { Route } from 'react-router-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
 import Home from './pages/Home/index';
 import TimeTrackDashboard from './pages/Projects/FMSolution/TimeTrackDashboard/index';
 
 function App() {
   return (
-    <div>
-      <Route path="/" exact component={Home} />
-      <Route path="/time-tracking-dashboard" component={TimeTrackDashboard} />
-    </div>
+    <HashRouter basename="/">
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/time-tracking-dashboard">TimeTracking</Link></li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={TimeTrackDashboard} />
+      </div>
+    </HashRouter>
   );
 }
 
